8890793b3b923a1c2360633823024ca97fb3ec4d
jean.daniel.michaud <jean.daniel.michaud@d37e9d85-a336-0410-8c01-ed6d7bde750b>
2007-08-02 07:27:14 +0000

    Source and test compiling and working.

--- trunk/test/basic_test.cpp
+++ trunk/test/basic_test.cpp
@@ -13,14 +13,29 @@
 #include <iostream>
 #include <command_line_interpreter.hpp>
 
+namespace po = boost::program_options;
+
+void handler(const std::vector<std::string> &parameters)
+{
+  for (std::vector<std::string>::const_iterator it = parameters.begin();
+       it != parameters.end();
+       ++it)
+    std::cout << *it << std::endl;
+}
+
+void exit_(unsigned int code = 0)
+{
+  exit(code);
+}
+
 int main(int argc, char **argv)
 {
-  options_description desc;
+  boost::cli::commands_description desc;
   desc.add_options()
-      ("handler", value< std::vector<std::string> >()->notifier(&handler)->multitoken())
-      ("exit", value< unsigned int >()->notifier(&exit_))
-      ;
+    ("handler", po::value< std::vector<std::string> >()->notifier(&handler)->multitoken())
+    ("exit", po::value< unsigned int >()->notifier(&exit_))
+    ;
 
-  command_line_interpreter cli(desc);
+  boost::cli::command_line_interpreter cli(desc);
   cli.interpret(std::cin);
 }
